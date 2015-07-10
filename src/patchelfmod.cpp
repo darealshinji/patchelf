5ec863f5c868b2b9340ea7e162d2598b3acb31f6
darealshinji <djcj@gmx.de>
2015-07-11 00:24:46 +0200

    Raise maximum file size to work around a PIC executable issue

--- src/patchelfmod.cpp
+++ src/patchelfmod.cpp
@@ -101,7 +101,7 @@ static void readFile(string fileName, mode_t * fileMode)
 		error("stat");
 	fileSize = st.st_size;
 	*fileMode = st.st_mode;
-	maxSize = fileSize + 8 * 1024 * 1024;
+	maxSize = fileSize + 32 * 1024 * 1024;
 
 	contents = (unsigned char *)malloc(fileSize + maxSize);
 	if (!contents)
